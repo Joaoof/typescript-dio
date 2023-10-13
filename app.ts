4bd152539860ae03e6e47a50ab6b77b105230d66
Joaoof <joaodeus400@gmail.com>
2023-10-13 18:32:37 -0300

    objetos e interfaces

--- app.ts
+++ app.ts
@@ -3,3 +3,35 @@ const soma = (a: number, b: number) => {
 }
 
 soma(10, 30)
+
+// Objetos e interfaces
+
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao?: string // "?" torna a propiedade opcional
+}
+
+const pessoa: Pessoa = {
+    nome: 'João',
+    idade: 19
+}
+
+const outraPessoa: Pessoa = {
+    nome: 'Paulo',
+    idade: 26,
+    profissao: 'Desenvolvedor'
+}
+
+const arrayPessoa: Array<Pessoa> = [
+    pessoa,
+    outraPessoa
+]
+
+const arrayNum: number[] = [
+    1, 2, 5
+]
+
+const arrayString: Array<string> = [
+    '2', '3', '4'
+]
