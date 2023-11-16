698df8259487bb513c299d856251b74a76ec7eed
Joaoof <joaodeus400@gmail.com>
2023-11-16 10:34:43 -0300

    herança

--- heranca.ts
+++ heranca.ts
@@ -2,22 +2,37 @@ class Count {
     number: number
     title: string
 
-    constructor(number: number, title: string) {
-        this.number = number
+    constructor(title: string) {
+        this.number = this.generateAccountNumber()
         this.title = title
     }
+ 
+    generateAccountNumber():number {
+        return Math.floor(Math.random()* 100000)+1
+    } // tiro o this.number pois essa função ja gera um numero
 }
 
 class CountPF extends Count {
+    cpf: number
 
+    constructor(cpf: number, title: string){
+        super(title)
+        this.cpf = cpf
+    }
 }
 
 class CountPJ extends Count {
+    cpnj: number
 
+    constructor(cnpj: number, title: string){
+        super(title)
+        this.cpnj = cnpj
+    }
 }
 
 const cont = new CountPF(1234, 'teste')
 const cont2 = new CountPJ(1234, 'teste')
 console.log(cont);
+console.log(cont2);
 
 
